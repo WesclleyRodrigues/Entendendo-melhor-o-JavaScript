532528c8c146680c0549837c733699295ea53117
Wesclley <wesclley.rodrigues@gmail.com>
2023-02-09 23:25:07 -0300

    Entendendo JavaScript

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,2 +1,9 @@
-alert("Entendendo melhor o JavaScript!");
-window.confirm("Preste muita atenção para aprender Javascript");
+alert("Acender Lampada");
+
+function nomeDaFuncao(mensagem) {
+    alert(mensagem);
+}
+
+function acenderLampada() {
+    document.getElementById("imgLampada").scr = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.softicons.com%2Fobject-icons%2Fworked-awhile-lights-icons-by-artbees%2Fon-lamp-icon&psig=AOvVaw1AJftQap7GBvci8dEiF6AX&ust=1676082110786000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiNldHyif0CFQAAAAAdAAAAABAE";
+}
