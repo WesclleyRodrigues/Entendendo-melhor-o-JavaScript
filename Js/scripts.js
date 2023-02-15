d35f04cb2fd04cc10e8e62f73379155ce0817db5
Wesclley <wesclley.rodrigues@gmail.com>
2023-02-14 22:57:37 -0300

    Como inserir JavaScript

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,9 +1,1 @@
-alert("Acender Lampada");
-
-function nomeDaFuncao(mensagem) {
-    alert(mensagem);
-}
-
-function acenderLampada() {
-    document.getElementById("imgLampada").scr = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.softicons.com%2Fobject-icons%2Fworked-awhile-lights-icons-by-artbees%2Fon-lamp-icon&psig=AOvVaw1AJftQap7GBvci8dEiF6AX&ust=1676082110786000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiNldHyif0CFQAAAAAdAAAAABAE";
-}
+console.log("Este e o Script do head!");
