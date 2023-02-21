fa18c086cf5cc6b512ae82df894f4bf7a8a8056a
Wesclley <wesclley.rodrigues@gmail.com>
2023-02-21 19:42:58 -0300

    Tipos de Variáveis

---
+++ string/js/scripts.js
@@ -0,0 +1,8 @@
+var nome = "Wesclley";
+var sobrenome = "Jesus";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+console.log(nomeCompleto);
---
+++ variaveis/js/scripts.js
@@ -0,0 +1,21 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'Wesclley';
+
+console.log(teste);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = "Wesclley";
+
+console.log(meuNome);
